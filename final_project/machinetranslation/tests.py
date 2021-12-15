942ef603b5e1ca107b859403f5f8bd3ed147b37e
Quijotengineer <inigo.monreal@bath.edu>
2021-12-15 16:13:07 +0000

    implement tests

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -6,9 +6,15 @@ class TestEnglishToFrench(unittest.TestCase):
         self.assertIsNotNone(self)
 
     def test_Hello(self):
+        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
 
 class TestFrenchToEnglish(unittest.TestCase):
 
     def test_NotNull(self):
-        
+        self.assertIsNotNone(self)
+
     def test_Bonjour(self):
+        self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
+
+if __name__ == '__main__'
+    unittest.main()
