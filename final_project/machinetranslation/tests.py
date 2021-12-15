9996844410b06e8411bdbac1370a5f1b26fbee48
Quijotengineer <inigo.monreal@bath.edu>
2021-12-15 16:57:25 +0000

    Need to check for input, not output

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -4,7 +4,7 @@ from translator import englishToFrench, frenchToEnglish
 class TestEnglishToFrench(unittest.TestCase):
     
     def test_NotNull(self):
-        self.assertIsNotNone(englishText)
+        self.assertIsNotNone(englishToFrench.locals().get("englishText"))
 
     def test_Hello(self):
         self.assertEqual(englishToFrench('Hello'), 'Bonjour')
@@ -12,7 +12,7 @@ class TestEnglishToFrench(unittest.TestCase):
 class TestFrenchToEnglish(unittest.TestCase):
 
     def test_NotNull(self):
-        self.assertIsNotNone(frenchText)
+        self.assertIsNotNone(frenchToEnglish.locals().get("frenchText"))
 
     def test_Bonjour(self):
         self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
